25ef96563889c0abfbab51a7cd654f09ebc14296
Vladislav Voitov <dobrihlopez@gmail.com>
2023-02-03 22:28:07 +0300

    add fragments and an album mode

--- app/src/main/java/com/voitov/cryptoapp/presentation/CoinDetailsActivity.kt
+++ app/src/main/java/com/voitov/cryptoapp/presentation/CoinDetailsActivity.kt
@@ -3,14 +3,11 @@ package com.voitov.cryptoapp.presentation
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import com.bumptech.glide.Glide
+import com.voitov.cryptoapp.R
 import com.voitov.cryptoapp.databinding.ActivityCoinDetailsBinding
 
 class CoinDetailsActivity : AppCompatActivity() {
-    private lateinit var viewModel: CoinPriceListViewModel
     private val binding by lazy {
         ActivityCoinDetailsBinding.inflate(layoutInflater)
     }
@@ -19,25 +16,15 @@ class CoinDetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        viewModel = ViewModelProvider(this).get(CoinPriceListViewModel::class.java)
-
         val fromSymbol = intent.getStringExtra(EXTRA_FROM_SYMBOL) ?: EMPTY_SYMBOL
 
-        viewModel.getCoinDetails(fromSymbol).observe(this) {
-            Log.d(TAG, it.toString())
-            with(binding) {
-                textViewPriceValue.text = it.price.toString()
-                textViewMinValueForWholeDay.text = it.lowDay.toString()
-                textViewFromSymbol.text = it.fromSymbol
-                textViewToSymbol.text = it.toSymbol
-                textViewMaxValueForWholeDay.text = it.highDay.toString()
-                textViewLastDeal.text = it.lastMarket
-                textViewLastUpdateTime.text = it.lastUpdate
-
-                Glide.with(this@CoinDetailsActivity)
-                    .load(it.imageUrl)
-                    .into(imageViewCoinLogo)
-            }
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(
+                    R.id.fragmentContainerCoinDetails,
+                    CoinDetailsFragment.newInstance(fromSymbol)
+                )
+                .commit()
         }
     }
 
--- app/src/main/java/com/voitov/cryptoapp/presentation/CoinPriceListActivity.kt
+++ app/src/main/java/com/voitov/cryptoapp/presentation/CoinPriceListActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.voitov.cryptoapp.R
 import com.voitov.cryptoapp.databinding.ActivityCoinPriceListBinding
 import com.voitov.cryptoapp.domain.entities.CoinInfo
 import com.voitov.cryptoapp.presentation.adapters.CoinInfoListAdapter
@@ -13,6 +14,7 @@ class CoinPriceListActivity : AppCompatActivity() {
     private val TAG = "CoinPriceListActivity"
 
     private lateinit var viewModel: CoinPriceListViewModel
+    private lateinit var adapter: CoinInfoListAdapter
     private val binding by lazy {
         ActivityCoinPriceListBinding.inflate(layoutInflater)
     }
@@ -20,25 +22,58 @@ class CoinPriceListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        setupRecyclerView()
+        setupViewModel()
+    }
 
-        viewModel = ViewModelProvider(this).get(CoinPriceListViewModel::class.java)
-        val adapter = CoinInfoListAdapter(this)
+    private fun setupRecyclerView() {
+        adapter = CoinInfoListAdapter(this)
         binding.recyclerViewCoins.adapter = adapter
         adapter.onClickListener = object : CoinInfoListAdapter.OnClickListener {
             override fun onClick(coinInfo: CoinInfo) {
                 Log.d(TAG, coinInfo.toString())
-                startActivity(
-                    CoinDetailsActivity.newIntent(
-                        this@CoinPriceListActivity,
-                        coinInfo.fromSymbol
-                    )
-                )
+                launchInCorrectMode(coinInfo.fromSymbol)
             }
         }
+    }
+
+    private fun setupViewModel() {
+        viewModel = ViewModelProvider(this).get(CoinPriceListViewModel::class.java)
+
 
         viewModel.getCoinData().observe(this, Observer {
             Log.d(TAG, it.toString())
             adapter.submitList(it)
         })
+    }
+
+    private fun launchInCorrectMode(fromSymbol: String) {
+        if (isOnePaneMode()) {
+            launchNewScreen(fromSymbol)
+        } else {
+            launchInTabletMode(fromSymbol)
+        }
+    }
+
+    private fun isOnePaneMode() = binding.fragmentContainerSecondPart == null
+
+    private fun launchInTabletMode(fromSymbol: String) {
+        supportFragmentManager.popBackStack()
+        supportFragmentManager.beginTransaction()
+            .addToBackStack(null)
+            .replace(
+                R.id.fragmentContainerSecondPart,
+                CoinDetailsFragment.newInstance(fromSymbol)
+            )
+            .commit()
+    }
+
+    private fun launchNewScreen(fromSymbol: String) {
+        startActivity(
+            CoinDetailsActivity.newIntent(
+                this@CoinPriceListActivity,
+                fromSymbol
+            )
+        )
     }
 }
