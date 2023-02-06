f65d1af974c764aed59fc05ee158d99a495ca449
Vladislav Voitov <dobrihlopez@gmail.com>
2023-02-06 23:28:06 +0300

    add worker factory without dependency injection

---
+++ app/src/main/java/com/voitov/cryptoapp/data/workers/RefreshDataWorkerFactory.kt
@@ -0,0 +1,23 @@
+package com.voitov.cryptoapp.data.workers
+
+import android.content.Context
+import androidx.work.ListenableWorker
+import androidx.work.WorkerFactory
+import androidx.work.WorkerParameters
+import com.voitov.cryptoapp.data.database.CoinInfoDao
+import com.voitov.cryptoapp.data.mapper.CoinInfoMapper
+import com.voitov.cryptoapp.data.network.ApiService
+
+class RefreshDataWorkerFactory(
+    private val apiService: ApiService,
+    private val dao: CoinInfoDao,
+    private val mapper: CoinInfoMapper,
+) : WorkerFactory() {
+    override fun createWorker(
+        appContext: Context,
+        workerClassName: String,
+        workerParameters: WorkerParameters
+    ): ListenableWorker {
+        return RefreshDataWorker(appContext, workerParameters, apiService, dao, mapper)
+    }
+}
